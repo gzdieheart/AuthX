a641c6ccd1f6ab9909951b946620b03a1feef11d
gzdieheart <dieheart@126.com>
2024-12-05 13:52:33 +0800

    remove jpa component

--- src/main/java/org/gzdieheart/authx/entities/User.java
+++ src/main/java/org/gzdieheart/authx/entities/User.java
@@ -1,74 +1,5 @@
 package org.gzdieheart.authx.entities;
 
-/*import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-*/
-/**
- * @author hyj
- * @version 1.0
- * @date  2024/11/29
- * 用户实体
- */
-
-/*@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "user")
-public class User implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private Role role;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        //return null;
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getUsername() {
-        // email in our case
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}*/
 import java.util.Collection;
 import java.util.List;
 
@@ -84,6 +15,13 @@ import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+
+/**
+ * @author hyj
+ * @version 1.0
+ * @date  2024/11/29
+ * 用户实体
+ */
 
 @Data
 @Builder
@@ -107,13 +45,11 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        //return null;
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getUsername() {
-        // email in our case
         return email;
     }
 
